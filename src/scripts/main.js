252882c7e5ce85185b0a6c1e86d029842d35e5aa
martineq <martineq@users.noreply.github.com>
2022-05-17 15:39:47 -0300

    use of cloneNode() instead of importNode()

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,5 @@
+
+////@ts-check // TODO: Ver uso de @ts-check para archivos JS, ya que da advertencias propias de TypeScript
 "use strict";
 
 // Función para agregar la cabecera a la tabla
@@ -16,10 +18,10 @@ function addHeader() {
 							templateHeaderN8:"NCB Duplicado",
 							templateHeaderN9:"Monto Comprobante B",
 							templateHeaderN10:"Fecha Comprobante B"};
+							 
+	// Creo una copia de la cabecera a insertar 
+	const documentHeader = headerTemplate.content.cloneNode(true);	// Devuelve un duplicado del nodo en el que este método fue llamado. Parámetros: externalNode -> "deep" (Opcional) true si los hijos del nodo también deben ser clonados, o false para clonar únicamente al nodo.
 
-	// Creo una copia de la cabecera a insertar
-	const documentHeader = document.importNode(headerTemplate.content, true); // Crea una copia de un nodo desde un documento externo para ser insertado en el documento actual. Parámetros: externalNode -> El nodo externo a ser importado, deep -> Un booleano que indica si los descendientes del nodo deben ser importados también.
-	
 	// Cargo los datos de la cabecera genérica
 	for(const hd in headerData) { 
 		documentHeader.querySelector("#"+hd).innerText = headerData[hd];
@@ -29,8 +31,8 @@ function addHeader() {
 	listHeader.appendChild(documentHeader);	// Agrega un nuevo nodo al final de la lista de un elemento hijo de un elemento padre especificado.
 }
 
-// Función para agregar registros
-function addDocument() {
+// Función para agregar registros con datos en la tabla
+function addRows() {
 	const documentList = document.querySelector(".document-list");			// Obtengo acceso a la tabla de datos (el tbody)
 	const documentTemplate = document.querySelector("#document-template");	// Obtengo acceso al template de registro para la tabla de datos
 	
@@ -49,8 +51,8 @@ function addDocument() {
 	// Repito la secuencia para varios registros
 	for (let i = 0; i < 10; i++) {
 		// Creo una copia de un registro a insertar
-		const documentElement = document.importNode(documentTemplate.content, true); 
-		
+		const documentElement = documentTemplate.content.cloneNode(true);
+
 		// Cargo los datos del registro genérico
 		for(const dd in documentData) { 
 			documentElement.querySelector("#"+dd).innerText = documentData[dd];
@@ -61,5 +63,16 @@ function addDocument() {
 	} 	
 }
 
+// Función para agregar en una tabla, la cabecera y el cuerpo con los datos
+function addTable() {
+	// Verifica si el browser soporta la funcionalidad <template> de HTML, buscando la existencia del atributo 'content'
+	if ( 'content' in document.createElement('template')) {
+		addHeader(); 
+		addRows();
+	} else {
+		console.error("El browser no soporta la funcionalidad <template> de HTML.");
+	}
+}
+
 // Agrego un listener el cual se activará al cargar completamente el docummento HTML 
-document.addEventListener("DOMContentLoaded", function(event) { addHeader(); addDocument(); } );
+document.addEventListener("DOMContentLoaded", function(event) { addTable(); } );
